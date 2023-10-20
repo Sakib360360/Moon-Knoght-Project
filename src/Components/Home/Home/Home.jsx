89d3729dd7207058ffaeff72c69f36893e8c29a5
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-10-20 19:11:41 +0600

    advisory added

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -3,14 +3,17 @@ import Banner from "../Banner/Banner";
 import Thankful from "../Thankful/Thankful";
 import Wearable from "../Wearable/Wearable";
 import StartedSection from "./StartedSection";
+import AdvisoryBoard from "./AdvisoryBoard";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Wearable></Wearable>
-      <Thankful></Thankful>
       <StartedSection></StartedSection>
+      <AdvisoryBoard></AdvisoryBoard>
+      <Thankful></Thankful>
+     
     </div>
   );
 };
