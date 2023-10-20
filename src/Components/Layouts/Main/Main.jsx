a726926d0b6f34d0c47889cd4ec8d9d0250fcd56
mdjewel999 <mdjewel878761@gmail.com>
2023-10-20 16:48:31 +0600

    new branch

--- src/Components/Layouts/Main/Main.jsx
+++ src/Components/Layouts/Main/Main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+
 import { Outlet } from 'react-router-dom';
+import Footer from '../../page/Shared/Footer/Footer';
 
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
